1df08d6a0ca9f73fc174584abfbc7c1391828c32
Luis Gonzalo <lgonzalo@gmail.com>
2020-02-14 18:42:04 +0100

    Added failing test

---
+++ Backend/PerfectChannel.WebApi.Test/TodoServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PerfectChannel.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectChannel.WebApi.Test
+{
+    [TestFixture]
+    public class TodoServiceTests
+    {
+        [Test]
+        public async Task WhenTodoListCreated_GivenNothingAdded_ThenResultIsAnEmptyListAsync()
+        {
+            // Arrange
+            var todoService = new TodoService();
+
+            // Act
+            var todoList = await todoService.GetAllTodoItemsAsync();
+
+            // Assert
+            Assert.IsNotNull(todoList, "A new todo list cannot be null");
+            Assert.IsTrue(todoList.Length == 0, "A new todo list must be empty");
+        }
+    }
+}
